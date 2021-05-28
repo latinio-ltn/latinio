d3308f9fdd0161a46fcadc641672b331d741a42c
Latinio LNT <83830338+latinio-ltn@users.noreply.github.com>
2021-05-28 13:30:33 -0500

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2018 - 2021 The Latinio developers (Blockchain Technology SAS)
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -1546,7 +1547,7 @@ bool BindListenPort(const CService &addrBind, string& strError)
     {
         int nErr = WSAGetLastError();
         if (nErr == WSAEADDRINUSE)
-            strError = strprintf(_("Unable to bind to %s on this computer. BitcoinTrust is probably already running."), addrBind.ToString());
+            strError = strprintf(_("Unable to bind to %s on this computer. Latinio is probably already running."), addrBind.ToString());
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %d, %s)"), addrBind.ToString(), nErr, strerror(nErr));
         LogPrintf("%s\n", strError);
